004471b6df255d98877f14e48160fa6069d5c9fd
P0ntus <Pontustobiasson@gmail.com>
2018-02-09 13:13:50 +0100

    m

--- RBF Network.py
+++ RBF Network.py
@@ -15,7 +15,7 @@ class Node:
         self.y = y
         self.variance = v
     def __repr__(self):
-        return "<x:%s y:%s>" % (self.x, self.y)
+        return "<x:%s y:%s v:%s>" % (self.x, self.y, self.variance)
     def __str__(self):
         return "member of Test"
 
@@ -45,17 +45,36 @@ sin_training_output_pattern = list(map(sin_function, sin_training_input_pattern)
 sin_test_input_pattern = np.asarray(np.arange(x_lower_interval + 0.05, x_upper_interval, step_length))
 sin_test_output_pattern = list(map(sin_function, sin_test_input_pattern))
 
+
 # Initiate RBF nodes
 NUM_NODES_ROW = 4
-NUM_NODES_COL = 2
-variance = 0.25
+NUM_NODES_COL = 1
+variance = 1.25
+mu, sigma = 0, 0.1 # used for weight initialization
 RBF_Nodes = []
+weight = []
 for c in range(0, NUM_NODES_COL):
 	for r in range(0, NUM_NODES_ROW):
 		x = (x_lower_interval + ((x_upper_interval - x_lower_interval)/NUM_NODES_ROW/2)) + r * ((x_upper_interval - x_lower_interval)/NUM_NODES_ROW)
 		y = (y_lower_interval + ((y_upper_interval - y_lower_interval)/NUM_NODES_COL/2)) + c * ((y_upper_interval - y_lower_interval)/NUM_NODES_COL)
+		weight.append(np.random.normal(mu, sigma, 1)[0])
+		#weight = 1
 		RBF_Nodes.append(Node(x, y, variance))
+		
+# Calculate phi
+phi = np.zeros((len(sin_training_input_pattern), len(RBF_Nodes)))
+for p in range (0, len(sin_training_input_pattern)):
+	for n in range (0, len(RBF_Nodes)):
+		phi[p][n] = transfer_function(sin_training_input_pattern[p], RBF_Nodes[n].x, RBF_Nodes[n].variance)
 
+# Calculate weights (THE ERROR IS CURRENTLY NOT DECREASING)
+for p in range (0, 10000):
+	output_pattern = np.sum(phi * weight, axis = 1)
+	print(squared_error(sin_training_output_pattern, output_pattern))
+	weight += 0.001 * np.linalg.solve(np.dot(np.transpose(phi), (phi * weight)), np.dot(np.transpose(phi), (sin_training_output_pattern)))
+	
+	
+'''	
 # Plot function and nodes
 X = []
 Y = []
@@ -73,5 +92,5 @@ for circle in Circles:
 	ax.add_artist(circle)
 
 plt.show()
-
+'''
 
