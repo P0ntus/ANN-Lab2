87f77bf0fce76a4e0df18b35cb21a7b3237939cc
Pontus Tobiasson <pontob@kth.se>
2018-02-23 17:32:02 +0100

    .

--- RBF Network.py
+++ RBF Network.py
@@ -7,11 +7,13 @@ np.set_printoptions(threshold=np.nan) #Always print the whole matrix
 
 x_lower_interval = 0
 x_upper_interval = 2*math.pi
-y_lower_interval = -1
-y_upper_interval = 1
+y_lower_interval = -2
+y_upper_interval = 2
 step_length = 0.1
 
-learning_rate = 0.0001
+learning_rate = 0.01
+
+random.seed(a=None)
 
 # Class that represents a 2d input and what type it should be classified as
 class Node:
@@ -39,6 +41,10 @@ def absolute_residual_error(expected, predicted):
 # We use Gaussian RBF's with the following transfer function
 def transfer_function(x, position, variance):
 	return (math.exp((-(x - position)**2) / (2*(variance**2))))
+
+# We use Gaussian RBF's with the following transfer function
+def transfer_function_2d(x, y, position_x, position_y, variance):
+	return (math.exp((-(euclidean_distance(x, y, position_x, position_y))**2) / (2*(variance**2))))
 
 def euclidean_distance(x1, y1, x2, y2):
 	return (math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2)))
@@ -77,7 +83,19 @@ def shuffle(A, B):
 		A[i] = temp_A[C[i]]
 		B[i] = temp_B[C[i]]
 
-for nodes in range(64, 65):
+# Adds shuffles pattern A and B
+def shuffle_3(A, B, C):
+	temp_A = np.copy(A)
+	temp_B = np.copy(B)
+	temp_C = np.copy(B)
+	D = np.asarray(np.arange(0, len(A), 1))
+	random.shuffle(D)
+	for i in range(0, len(D)):
+		A[i] = temp_A[D[i]]
+		B[i] = temp_B[D[i]]
+		C[i] = temp_C[D[i]]
+
+for nodes in range(33, 34):
 	# Generate function data
 
 	# SIN DATA----------------------------------------------------------------------------------------------------------
@@ -103,28 +121,42 @@ for nodes in range(64, 65):
 	#square_test_output_pattern = list(map(square_function, square_test_input_pattern))
 	# SQUARE DATA-------------------------------------------------------------------------------------------------------
 
+	errors = []
+	RANDOM_errors = []
 #for nodes in range(0, 101):
 	# Initiate RBF nodes and WEIGHTS
 	NUM_NODES_ROW = nodes # Using len(sin_training_output_pattern) or len(square_training_output_pattern) gives good results
-	NUM_NODES_COL = 1
+	NUM_NODES_COL = 3
 	variance = 0.5
 	mu, sigma = 0, 0.1 # used for weight initialization
 	RBF_Nodes = []
 	weight = []
+	RANDOM_RBF_Nodes = []
 	for c in range(0, NUM_NODES_COL):
 		for r in range(0, NUM_NODES_ROW):
 			x = (x_lower_interval + ((x_upper_interval - x_lower_interval)/NUM_NODES_ROW/2)) + r * ((x_upper_interval - x_lower_interval)/NUM_NODES_ROW)
-			y = (y_lower_interval + ((y_upper_interval - y_lower_interval)/NUM_NODES_COL/2)) + c * ((y_upper_interval - y_lower_interval)/NUM_NODES_COL)
+			y = (y_lower_interval/2 + ((y_upper_interval - y_lower_interval)/NUM_NODES_COL/2)) + c * ((y_upper_interval - y_lower_interval)/NUM_NODES_COL)
+			RANDOM_x = float(random.randint(x_lower_interval*1000, math.ceil(x_upper_interval)*1000))/1000
+			RANDOM_y = float(random.randint(y_lower_interval*1000, y_upper_interval*1000))/1000
+			#print(float(random.randint(x_lower_interval*1000, math.ceil(x_upper_interval)*1000))
+			#print(x, y)
 			weight.append(np.random.normal(mu, sigma, 1)[0])
 			RBF_Nodes.append(Node(x, y, variance))
+			RANDOM_RBF_Nodes.append(Node(RANDOM_x, RANDOM_y, variance))
 
 	# Calculate SIN phi
 	sin_train_phi = np.zeros((len(sin_training_input_pattern), len(RBF_Nodes)))
 	sin_test_phi = np.zeros((len(sin_training_input_pattern), len(RBF_Nodes)))
+	RANDOM_sin_train_phi = np.zeros((len(sin_training_input_pattern), len(RBF_Nodes)))
+	RANDOM_sin_test_phi = np.zeros((len(sin_training_input_pattern), len(RBF_Nodes)))
 	for p in range (0, len(sin_training_input_pattern)):
 		for n in range (0, len(RBF_Nodes)):
-			sin_train_phi[p][n] = transfer_function(sin_training_input_pattern[p], RBF_Nodes[n].x, RBF_Nodes[n].variance)
-			sin_test_phi[p][n] = transfer_function(sin_test_input_pattern[p], RBF_Nodes[n].x, RBF_Nodes[n].variance)
+			#sin_train_phi[p][n] = transfer_function(sin_training_input_pattern[p], RBF_Nodes[n].x, RBF_Nodes[n].variance)
+			#sin_test_phi[p][n] = transfer_function(sin_test_input_pattern[p], RBF_Nodes[n].x, RBF_Nodes[n].variance)
+			sin_train_phi[p][n] = transfer_function_2d(sin_training_input_pattern[p], sin_training_output_pattern[p], RBF_Nodes[n].x, RBF_Nodes[n].y, RBF_Nodes[n].variance)
+			sin_test_phi[p][n] = transfer_function_2d(sin_test_input_pattern[p], sin_test_output_pattern[p], RBF_Nodes[n].x, RBF_Nodes[n].y, RBF_Nodes[n].variance)
+			RANDOM_sin_train_phi[p][n] = transfer_function_2d(sin_training_input_pattern[p], sin_training_output_pattern[p], RANDOM_RBF_Nodes[n].x, RANDOM_RBF_Nodes[n].y, RANDOM_RBF_Nodes[n].variance)
+			RANDOM_sin_test_phi[p][n] = transfer_function_2d(sin_test_input_pattern[p], sin_test_output_pattern[p], RANDOM_RBF_Nodes[n].x, RANDOM_RBF_Nodes[n].y, RANDOM_RBF_Nodes[n].variance)
 	'''	
 	# Calculate SQUARE phi
 	square_train_phi = np.zeros((len(square_training_input_pattern), len(RBF_Nodes)))
@@ -152,25 +184,32 @@ for nodes in range(64, 65):
 	# Delta rule
 	# Initiate weights
 	sin_sequential_weight  = []
+	RANDOM_sin_sequential_weight  = []
 	#square_sequential_weight = []
 	#sin_batch_weight = []
 	#square_batch_weight = []
 	for i in range(0, len(weight)):
 		sin_sequential_weight.append(weight[i])
+		RANDOM_sin_sequential_weight.append(weight[i])
 		#square_sequential_weight.append(weight[i])
 		#sin_batch_weight.append(weight[i])
 		#square_batch_weight.append(weight[i])
 
-	epochs = 10000000
+	epochs = 10000
 	
 	# Sequential Delta rule--------------------------------------------------------------------------------------------------------------------------------------------
 	# SIN
 	for i in range(0, epochs):
-		shuffle(sin_training_output_pattern, sin_train_phi)
+		shuffle_3(sin_training_output_pattern, sin_train_phi, RANDOM_sin_train_phi)
 		for o in range(0, len(sin_training_output_pattern)):
 			sin_sequential_weight = sin_sequential_weight + (learning_rate*(sin_training_output_pattern[o] - np.sum(sin_train_phi[o] * sin_sequential_weight))*(sin_train_phi[o]))
+			RANDOM_sin_sequential_weight = RANDOM_sin_sequential_weight + (learning_rate*(sin_training_output_pattern[o] - np.sum(RANDOM_sin_train_phi[o] * RANDOM_sin_sequential_weight))*(RANDOM_sin_train_phi[o]))
 		sin_sequential_output_pattern = np.sum(sin_test_phi * sin_sequential_weight, axis = 1)
+		RANDOM_sin_sequential_output_pattern = np.sum(RANDOM_sin_test_phi * RANDOM_sin_sequential_weight, axis = 1)
+		errors.append(squared_error(sin_test_output_pattern, sin_sequential_output_pattern))
 		print("Epoch:", i, "SIN Sequential Delta rule error:", squared_error(sin_test_output_pattern, sin_sequential_output_pattern))
+		RANDOM_errors.append(squared_error(sin_test_output_pattern, RANDOM_sin_sequential_output_pattern))
+		print("Epoch:", i, "RANDOM SIN Sequential Delta rule error:", squared_error(sin_test_output_pattern, RANDOM_sin_sequential_output_pattern))
 	#print("Nodes:", nodes, "SIN Sequential Delta rule error:", squared_error(sin_test_output_pattern, sin_sequential_output_pattern))
 	
 	'''
@@ -206,10 +245,10 @@ for nodes in range(64, 65):
 	# Batch Delta rule-------------------------------------------------------------------------------------------------------------------------------------------------
 	'''
 	
-'''
+
 # Plot
 ax = plt.gca()
-
+'''
 # Plot nodes
 X = []
 Y = []
@@ -222,12 +261,14 @@ for node in RBF_Nodes:
 ax.plot(X, Y, "ro")
 for circle in Circles:
 	ax.add_artist(circle)
-import random
+'''
 # Plot data
-ax.plot(sin_test_input_pattern, sin_sequential_output_pattern)
-ax.plot(sin_test_input_pattern, sin_test_output_pattern)
+#ax.plot(sin_test_input_pattern, sin_sequential_output_pattern)
+#ax.plot(sin_test_input_pattern, sin_test_output_pattern)
+ax.plot(errors)
+ax.plot(RANDOM_errors)
 
 plt.show()
-'''
+
 
 
